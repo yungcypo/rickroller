ed7db5f6c9d5aa1184f2d94991a513d74f85ae4e
cypo <80849671+yungcypo@users.noreply.github.com>
2022-03-25 14:25:32 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
-import webbrowser, time
+import webbrowser, time, csv
 
 # len taka srandicka :)
 
+time.sleep(2)
 webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
 t = time.gmtime()
+list = [
+    t.tm_mday,
+    t.tm_mon,
+    t.tm_year,
+    "-",
+    t.tm_hour,
+    t.tm_min,
+    t.tm_sec
+]
 
-x = ""
-x += str(t.tm_year)
-if t.tm_mon < 10:
-    x += "0" + str(t.tm_mon)
-else:
-    x += str(t.tm_mon)
-if t.tm_mday < 10:
-    x += "0" + str(t.tm_mday)
-else:
-    x += str(t.tm_mday)
-
-x += "-"
-
-if t.tm_hour < 10:
-    x += "0" + str(t.tm_hour + 1)
-else:
-    x += str(t.tm_hour + 1)
-if t.tm_min < 10:
-    x += "0" + str(t.tm_min)
-else:
-    x += str(t.tm_min)
-
-with open("file.txt", "a") as file:
-    file.write(x + "\n")
+with open("file.csv", "a", newline="") as file:
+    csv.writer(file).writerow(list)
